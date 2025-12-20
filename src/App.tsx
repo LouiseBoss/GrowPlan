21c9fb9900a8ecac421f77706695f92b190afaaa
Louise Bosson <louisebosson@gmail.com>
2025-12-20 01:06:29 +0100

    deploy again

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import GardenPage from "./pages/MyGardenPage";
 import WishlistPage from "./pages/WishlistPage";
 import ProfilePage from "./pages/ProfilePage";
-import Footer from "./pages/partials/Footer";
+import Footer from "./pages/partials/footer";
 import "../src/assets/scss/main.scss";
 
 function App() {
